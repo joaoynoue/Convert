340e89c1526a5dc3ee78b41c147cd90dc0174dae
joaoynoue <joaoynoueoficial@gmail.com>
2025-08-11 12:29:03 -0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog
-
+from tkinter import filedialog, messagebox
+import os
+import fitz
 
 pdf = None
 
@@ -11,18 +12,62 @@ janela_principal.geometry("500x500")
 janela_principal.resizable(False, False)
 janela_principal.grid_rowconfigure(0, weight=1)
 janela_principal.grid_columnconfigure(0, weight=1)
+status_label = tk.Label(janela_principal,text='Nenhum arquivo selecionado')
+status_label.grid(row=1, column=0, columnspan=2, sticky="ew", padx=10)
 
 #Função Selecionar  
 def selecionar_pdf():
     global pdf
     pdf = filedialog.askopenfilename(
+        parent=janela_principal,
         title="Selecionar Um Arquivo PDF",
-        filetypes=[("Arquivos PDF", "*.pdf")])
+        filetypes=[("Arquivos PDF", "*.pdf")]
+    )
     
-    if pdf is True:
-        print("Arquivo Selecionado!")
+    if pdf:
+        status_label.config(text="Arquivo selecionado!")
     else:
-        print("Nenhum arquivo selecionado")
+        status_label.config(text="Nenhum arquivo selecionado")
+        
+
+#Função converter
+
+def converter():
+
+    if not pdf:
+        messagebox.showwarning("Aviso", "Selecione um arquivo para poder converter")
+        return
+    
+    arquivo_salvo = filedialog.askdirectory(
+        parent=janela_principal, title="Selecione uma paste para salvar a conversão"
+
+    )
+
+    if not arquivo_salvo:
+        messagebox.showwarning("Aviso", "Selecione uma pasta para salvar os arquivos")
+        return 
+    
+    try:
+        doc = fitz.open(pdf)  
+        total_paginas = doc.page_count
+
+        for i, pagina in enumerate(doc, start=1):
+            pix = pagina.get_pixmap(dpi=300)  
+            nome_arquivo = os.path.join(arquivo_salvo, f"pagina_{i}.png")
+            pix.save(nome_arquivo)
+
+        
+        messagebox.showinfo("Sucesso", f"{total_paginas} página(s) convertida(s) para PNG com sucesso!")
+        doc.close()
+    except Exception as e:
+        messagebox.showerror("Erro", f"Falha ao converter: {e}")
+        
+        
+        
+        
+        
+    
+
 
 
 
@@ -34,7 +79,7 @@ botao_arquivo.grid(row=0, column=0, sticky="ew", padx=10, pady=20)
 
 #Botão da Conversão
 
-botao_converter = tk.Button(janela_principal, text="Converter",command="janela_confirmacao" )
+botao_converter = tk.Button(janela_principal, text="Converter",command=converter )
 botao_converter.grid(row=0, column=1, sticky="ew", padx=10, pady=20)
 
 
